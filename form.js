2a00bd2b1c2449db8b757633c0d04f33c369c847
ashin444 <93370941+ashin444@users.noreply.github.com>
2021-11-15 18:50:31 +0530

    validation
    form validation

---
+++ form.js
@@ -0,0 +1,24 @@
+function validate(){
+    let email = document.getElementById("email");
+    let error = document.getElementById("error");
+    let pwd = document.getElementById("pwd");
+  
+    let regexp = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/
+    if(regexp.test(email.value)){
+        error.innerHTML = "Valid";
+        error.style.color = "green";
+        return true;
+    }
+
+    
+    if(email.value.trim()==""||pwd.value.trim()==""){
+                alert("Fields cannot be empty");
+                return false;
+             }
+
+    else{
+        error.innerHTML = "invalid";
+        error.style.color = "red";
+        return false;
+    }
+}
